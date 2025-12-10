2049e2d292914a1e66e7a7034db99a2fe7815250
Shozab <shozabali06@gmail.com>
2025-12-10 20:02:09 +0500

    feat: add footer with creator information to Home and Secret pages

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,8 +3,14 @@ import FlashPaperCard from "../components/FlashPaperCard";
 
 const Home = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center p-4">
+    <div className="min-h-screen flex items-center justify-center p-4 relative">
       <FlashPaperCard />
+      <span className="absolute bottom-2 right-2 text-sm text-muted-foreground">
+        Made with ❤️ by{" "}
+        <a href="https://github.com/shozabali06" className="underline">
+          Shozab
+        </a>
+      </span>
     </div>
   );
 };
--- client/src/pages/Secret.jsx
+++ client/src/pages/Secret.jsx
@@ -4,8 +4,14 @@ import React from 'react'
 const Secret = () => {
   const id = window.location.pathname.split("/secret/")[1];
   return (
-    <div className="min-h-screen flex items-center justify-center p-4">
+    <div className="min-h-screen flex items-center justify-center p-4 relative">
       <RevealCard id={id} />
+      <span className="absolute bottom-2 right-2 text-sm text-muted-foreground">
+        Made with ❤️ by{" "}
+        <a href="https://github.com/shozabali06" className="underline">
+          Shozab
+        </a>
+      </span>
     </div>
   )
 }
