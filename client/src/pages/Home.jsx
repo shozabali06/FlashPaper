861241e5dcddc411a334a6591369d1874bb0027d
Shozab <shozabali06@gmail.com>
2025-12-10 20:26:21 +0500

    fix: update layout and formatting in Home and Secret pages

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,11 +3,15 @@ import FlashPaperCard from "../components/FlashPaperCard";
 
 const Home = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center p-4 relative">
+    <div className="min-h-svh flex items-center justify-center p-4 relative">
       <FlashPaperCard />
       <span className="absolute bottom-2 right-2 text-sm text-muted-foreground">
         Made with ❤️ by{" "}
-        <a href="https://github.com/shozabali06" target="_blank" className="underline">
+        <a
+          href="https://github.com/shozabali06"
+          target="_blank"
+          className="underline"
+        >
           Shozab
         </a>
       </span>
--- client/src/pages/Secret.jsx
+++ client/src/pages/Secret.jsx
@@ -1,19 +1,23 @@
-import RevealCard from '@/components/RevealCard';
-import React from 'react'
+import RevealCard from "@/components/RevealCard";
+import React from "react";
 
 const Secret = () => {
   const id = window.location.pathname.split("/secret/")[1];
   return (
-    <div className="min-h-screen flex items-center justify-center p-4 relative">
+    <div className="min-h-svh flex items-center justify-center p-4 relative">
       <RevealCard id={id} />
       <span className="absolute bottom-2 right-2 text-sm text-muted-foreground">
         Made with ❤️ by{" "}
-        <a href="https://github.com/shozabali06" target='_blank' className="underline">
+        <a
+          href="https://github.com/shozabali06"
+          target="_blank"
+          className="underline"
+        >
           Shozab
         </a>
       </span>
     </div>
-  )
-}
+  );
+};
 
-export default Secret
+export default Secret;
